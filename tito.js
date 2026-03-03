c02387edcf342145e7ca15ba22b682fb3d202294
1301315803-eng <1301315803@s.tusd1.org>
2026-03-03 18:37:24 +0000

    dodod

--- tito.js
+++ tito.js
@@ -3,19 +3,41 @@ console.log("script started");
 //Variable to track the number of clicks 
 let clicks = 0;
 
+//variable to track the number of the levels 
+let clickvalue = 1;
+
 //variable to store the click display h1
 let clickDisplay = document.getElementById("click-display")
 
+//variable to store the click display h1 element
+let levelDisplay = document.getElementById("level-display");
+
 //set initial text
 clickDisplay.innerText = "Clicks: " + clicks; 
+
+//set the initial text for level
+
 
 //called when the potato is clicked 
 function handleClick() {
     console.log("click");
 
     // Add one to click count
-    click = click + 1;
+    clicks = clicks + 1;
 
     //update display
-    clickDisplay.innerText = "clicks: " + clicks;
+    clickDisplay.innerText = "Clicks: " + clicks;
+
+    if (clicks == 10) {
+        alert("pop-up alert!");
+        levelDisplay.innerText = "leve: 2";
+    }
+    else if (clicks >=10) {
+        
+    }
+
+     else if (clicks < 10) {
+      levelDisplay.innerText
+    }
+
 }
